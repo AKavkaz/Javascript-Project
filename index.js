a73ba8b010db66a61201b5e25d34af06d694d79a
AKavkaz <andrewkhomoutov@hotmail.com>
2024-03-19 22:39:01 +0800

    Finished Project

---
+++ index.js
@@ -0,0 +1,38 @@
+function openMenu() {
+    document.body.classList += " menu--open";
+}
+  
+function closeMenu() {
+    document.body.classList.remove("menu--open");
+}
+
+
+
+const movieListEl = document.querySelector('.movie__wrapper')
+
+async function onSearchChange(event) {
+    const searchTerm = event.target.value
+    const movies = await fetch(`http://www.omdbapi.com/?s=${searchTerm}&apikey=fbb2e191`)
+    const moviesData = await movies.json()
+    movieListEl.innerHTML = moviesData.Search.map((movie) => movieHTML(movie)).join('')
+}
+
+function movieHTML(movie) {
+    return `<div class="movie">
+    <div class="movie__img">
+        <img src="${movie.Poster}" alt="">
+        <div class="movie__content">
+            <h1>${movie.Title}</h1>
+            <h1>${movie.Year}</h1>
+            <p><a onclick="movieInfo('${movie.imdbID}')">MORE INFO</a></p>
+        </div>
+    </div>
+</div>`
+}
+
+function movieInfo(imdbID) {
+    localStorage.setItem("imdbID", imdbID)
+    window.location.href = `${window.location.origin}/movies.html`
+}
+
+
---
+++ movies.js
@@ -0,0 +1,36 @@
+function movieInfo() {
+    const id = localStorage.getItem("imdbID");
+}
+  
+movieInfo();
+
+const id = localStorage.getItem("imdbID")
+
+async function movieDesc() {
+    const moviesInfo = await fetch(`http://www.omdbapi.com/?i=${localStorage.getItem("imdbID")}&apikey=fbb2e191`)
+    let moviesInfoData = await moviesInfo.json()
+    if (!Array.isArray(moviesInfoData)) {
+        moviesInfoData = [moviesInfoData];
+    }
+
+    const movieDescEl = document.querySelector('.container')
+    movieDescEl.innerHTML = moviesInfoData.map((movie) => moviesInfoHTML(movie))
+    console.log(moviesInfoData)
+}
+movieDesc()
+
+function moviesInfoHTML(movie) {
+    return `<div class="movie__img--wrapper">
+    <h1>${movie.Title}</h1>
+    <img src="${movie.Poster}" alt="">
+</div>
+<div class="movie__info--wrapper">
+    <h3><span class="red">Released: </span>${movie.Released}</h3>
+    <h3><span class="red">Actors: </span>${movie.Actors}</h3>
+    <h3><span class="red">Genre: </span>${movie.Genre}</h3>
+    <h3><span class="red">Director: </span>${movie.Director}</h3>
+    <h3><span class="red">Writer: </span>${movie.Writer}</h3>
+    <h3><span class="red">Language: </span>${movie.Language}</h3>
+    <h3><span class="red">Plot: </span>${movie.Plot}</h3>
+</div>`
+}
